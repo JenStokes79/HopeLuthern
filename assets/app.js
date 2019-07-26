00ea831ee2c71ffdc3f197be7eae12e1ba4d9dfd
Jennifer Stokes <jenniferstokes@jennifers-mbp.attlocal.net>
2019-07-25 20:49:12 -0700

    new design

---
+++ assets/app.js
@@ -0,0 +1,12 @@
+
+$(document).ready(function () {
+    $('.slider').bxSlider();
+});
+
+$('.bxslider').bxSlider({
+    auto: true,
+    autoControls: true,
+    stopAutoOnClick: true,
+    pager: true,
+    slideWidth: 1440
+});
